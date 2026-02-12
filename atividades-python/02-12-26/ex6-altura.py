b00b4a68f8ee0b36009a21bd84ad89d63d5fae94
WALDAIR OTTO MENEGASSI <waldair_menegassi@estudante.sesisenai.org.br>
2026-02-12 10:49:23 -0300

    ex6-1202

--- atividades-python/02-12-26/ex6-altura.py
+++ atividades-python/02-12-26/ex6-altura.py
@@ -1,3 +1,11 @@
 h = float(input("Digite a altura em cm: "))
 sexo = input("sexo (m ou f): ")
 
+if sexo.upper() == "M":
+    kgideal = (72.7*(h/100))-58
+    print("Peso ideal para uma homem de {:.2f}m é de {:.2f}kg".format((h/100),kgideal))
+elif sexo.upper() =="f":
+    kgideal = (62.1*h)-44.7
+    print("Peso ideal para uma mulher de {:.2f}m é de {:.2f}kg".format((h/100),kgideal))
+else:
+    print("Seu sexo não é sexual")
